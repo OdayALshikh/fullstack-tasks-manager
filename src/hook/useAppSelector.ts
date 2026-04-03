02670da666065e7b497ec0cf6153999f9fba8a64
OdayALshikh <ODAYSH056@gmail.com>
2026-04-03 21:54:56 +0200

    Create Register and Login pages structure

--- src/hook/useAppSelector.ts
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-import type { RootState } from "../App/store";
-
-export const useAppSelector = <TSelected>(
-  selector: (state: RootState) => TSelected,
-): TSelected => {
-  return useSelector(selector);
-};
